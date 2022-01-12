4fff342076d277df817e234e3d403ebe30687dff
xinyu <xinyu@test.com>
2022-01-12 22:58:45 +0800

    增加

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*!
+/*!src="assets/img/ipad.png"
 * Start Bootstrap - Grayscale v7.0.4 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
